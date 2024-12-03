4a63526f2f6cb88859c370b6ce6e9cd602128cc6
Sebastian Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-03 11:21:18 +0100

    Zig Day 3 2024 part 2

--- 2024/day03/zig/main.zig
+++ 2024/day03/zig/main.zig
@@ -43,6 +43,57 @@ fn part1(file: std.fs.File, allocator: Allocator) !u64 {
     return sum;
 }
 
+fn part2(file: std.fs.File, allocator: Allocator) !u64 {
+    var buffered_reader = std.io.bufferedReader(file.reader());
+    const reader = buffered_reader.reader();
+    const stat = try file.stat();
+
+    var sum: u64 = 0;
+    var enabled: bool = true;
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', stat.size)) |line| {
+        defer allocator.free(line);
+
+        var i: u64 = 0;
+        outer: while (i < line.len - 7) : (i += 1) {
+            const possible_length = if (line.len - i < 12) line.len - i else 12;
+            const slice = line[i..(i + possible_length)];
+
+            if (std.mem.eql(u8, slice[0..4], "do()"))
+                enabled = true;
+            if (std.mem.eql(u8, slice[0..7], "don't()"))
+                enabled = false;
+
+            if (!enabled)
+                continue :outer;
+
+            if (!std.mem.eql(u8, slice[0..4], "mul("))
+                continue;
+            var closing_index: ?u64 = null;
+            for (slice, 0..) |c, j| {
+                if (c == ')') {
+                    closing_index = j;
+                    break;
+                }
+            }
+            if (closing_index) |j| {
+                var args = std.mem.splitAny(u8, slice[4..j], ",");
+                const xs = args.next() orelse continue :outer;
+                if (xs.len == 0 or xs.len > 3) continue :outer;
+                const x = try std.fmt.parseInt(u64, xs, 10);
+
+                const ys = args.next() orelse continue :outer;
+                if (ys.len == 0 or ys.len > 3) continue :outer;
+                const y = try std.fmt.parseInt(u64, ys, 10);
+
+                if (args.next()) |_| continue :outer;
+
+                sum += x * y;
+            }
+        }
+    }
+    return sum;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
@@ -57,6 +108,11 @@ pub fn main() !void {
         defer file.close();
         std.debug.print("Part 1: {}\n", .{try part1(file, allocator)});
     }
+    {
+        const file = std.fs.cwd().openFile(filename, .{ .mode = .read_only }) catch return error.FileNotFound;
+        defer file.close();
+        std.debug.print("Part 2: {}\n", .{try part2(file, allocator)});
+    }
 }
 
 test "part 1" {
@@ -65,4 +121,12 @@ test "part 1" {
     const allocator = std.testing.allocator;
     const result = part1(file, allocator);
     try std.testing.expectEqual(161, result);
+}
+
+test "part 2" {
+    const file = std.fs.cwd().openFile("../../../inputs/2024/day03/test_2.txt", .{ .mode = .read_only }) catch return error.FileNotFound;
+    defer file.close();
+    const allocator = std.testing.allocator;
+    const result = part2(file, allocator);
+    try std.testing.expectEqual(48, result);
 }
