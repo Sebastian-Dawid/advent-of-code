c2be3409631d5bf34b6d279ae86cb0474f2eaf0a
Sebastian Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-06 09:45:43 +0100

    Zig Day 6 2024 part 1

--- 2024/day06/zig/main.zig
+++ 2024/day06/zig/main.zig
@@ -2,11 +2,27 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 const Map = struct {
+    const Direction = enum(u64) {
+        UP = 0,
+        RIGHT = 1,
+        DOWN = 2,
+        LEFT = 3,
+        fn advance(self: @This()) @This() {
+            switch (self) {
+                Direction.UP => return Direction.RIGHT,
+                Direction.RIGHT => return Direction.DOWN,
+                Direction.DOWN => return Direction.LEFT,
+                Direction.LEFT => return Direction.UP,
+            }
+        }
+    };
     const Position = struct { x: u64, y: u64 };
+    const Guard = struct { pos: Position, dir: Direction };
     allocator: Allocator,
     obstacles: std.ArrayList(Position),
     width: u64,
     height: u64,
+    guard: Guard,
 
     fn init(allocator: Allocator) @This() {
         var self: @This() = undefined;
@@ -14,9 +30,63 @@ const Map = struct {
         self.obstacles = std.ArrayList(Position).init(allocator);
         self.width = 0;
         self.height = 0;
+        return self;
     }
-    fn deinit(self: @This()) void {
+    fn deinit(self: *@This()) void {
         self.obstacles.deinit();
+    }
+
+    fn step(self: *@This()) bool {
+        switch (self.guard.dir) {
+            Direction.UP => self.guard.pos.y -= 1,
+            Direction.RIGHT => self.guard.pos.x += 1,
+            Direction.DOWN => self.guard.pos.y += 1,
+            Direction.LEFT => self.guard.pos.x -= 1,
+        }
+        if (cond: {
+            for (self.obstacles.items) |o| {
+                if (o.x == self.guard.pos.x and o.y == self.guard.pos.y) break :cond true;
+            }
+            break :cond false;
+        }) {
+            switch (self.guard.dir) {
+                Direction.UP => self.guard.pos.y += 1,
+                Direction.RIGHT => self.guard.pos.x -= 1,
+                Direction.DOWN => self.guard.pos.y -= 1,
+                Direction.LEFT => self.guard.pos.x += 1,
+            }
+            self.guard.dir = self.guard.dir.advance();
+        }
+        if (self.guard.pos.x >= self.width or self.guard.pos.y >= self.height)
+            return false;
+        return true;
+    }
+
+    fn print(self: *const @This()) void {
+        var i: u64 = 0;
+        while (i < self.height) : (i += 1) {
+            var j: u64 = 0;
+            while (j < self.width) : (j += 1) {
+                if (self.guard.pos.x == j and self.guard.pos.y == i) {
+                    switch (self.guard.dir) {
+                        Direction.UP => std.debug.print("^", .{}),
+                        Direction.RIGHT => std.debug.print(">", .{}),
+                        Direction.DOWN => std.debug.print("v", .{}),
+                        Direction.LEFT => std.debug.print("<", .{}),
+                    }
+                    continue;
+                }
+                for (self.obstacles.items) |o| {
+                    if (o.x == j and o.y == i) {
+                        std.debug.print("#", .{});
+                        break;
+                    }
+                } else {
+                    std.debug.print(".", .{});
+                }
+            }
+            std.debug.print("\n", .{});
+        }
     }
 };
 
@@ -26,16 +96,41 @@ fn part1(file: std.fs.File, allocator: Allocator) !u64 {
 
     const stat = try file.stat();
 
-    var map = std.ArrayList(u8).init(allocator);
+    var map = Map.init(allocator);
     defer map.deinit();
 
-    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', stat.size)) |line| {
+    var i: u64 = 0;
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', stat.size)) |line| : (i += 1) {
         defer allocator.free(line);
-        try map.appendSlice(line);
+        if (i == 0)
+            map.width = line.len;
+        for (line, 0..) |c, j| {
+            switch (c) {
+                '#' => try map.obstacles.append(.{ .x = j, .y = i }),
+                '^' => map.guard = .{ .pos = .{ .x = j, .y = i }, .dir = Map.Direction.UP },
+                '>' => map.guard = .{ .pos = .{ .x = j, .y = i }, .dir = Map.Direction.RIGHT },
+                'v' => map.guard = .{ .pos = .{ .x = j, .y = i }, .dir = Map.Direction.DOWN },
+                '<' => map.guard = .{ .pos = .{ .x = j, .y = i }, .dir = Map.Direction.LEFT },
+                else => {},
+            }
+        }
     }
-    std.debug.print("{any}\n", .{ map.items });
+    map.height = i;
 
-    return 0;
+    var visited = std.AutoHashMap(Map.Position, u64).init(allocator);
+    defer visited.deinit();
+    try visited.put(map.guard.pos, 1);
+    while (map.step()) {
+        _ = try visited.getOrPutValue(map.guard.pos, 1);
+        //std.debug.print("===\n", .{});
+        //map.print();
+    }
+
+    var sum: u64 = 0;
+    var it = visited.valueIterator();
+    while (it.next()) |v| sum += v.*;
+
+    return sum;
 }
 
 pub fn main() !void {
