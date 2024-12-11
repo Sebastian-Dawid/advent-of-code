55f5bc552751b5b8c7b2063c67b7c6a18b49972d
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-11 14:42:53 +0100

    Zig Day 11 2024 part 2
    Refactored part 1 to use fast solver from part 2

--- 2024/day11/zig/main.zig
+++ 2024/day11/zig/main.zig
@@ -1,46 +1,72 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-fn part1(file: std.fs.File, allocator: Allocator) !u64 {
+const Solver = struct {
+    const ValuePair = struct { value: u64, depth: u64 };
+    allocator: Allocator,
+    rocks: std.AutoHashMap(ValuePair, u64),
+
+    fn init(allocator: Allocator) @This() {
+        var self: @This() = undefined;
+        self.allocator = allocator;
+        self.rocks = @TypeOf(self.rocks).init(allocator);
+        return self;
+    }
+
+    fn deinit(self: *@This()) void {
+        self.rocks.deinit();
+    }
+
+    fn blinks(self: *@This(), value: u64, depth: u64) !u64 {
+        if (depth == 0) return 1;
+
+        if (self.rocks.get(.{ .value = value, .depth = depth })) |v| {
+            return v;
+        }
+
+        var count: u64 = undefined;
+        if (value == 0) {
+            count = try self.blinks(1, depth - 1);
+        } else if ((std.math.log10_int(value) + 1) % 2 == 0) {
+            const div = try std.math.powi(u64, 10, (std.math.log10_int(value)+1)/2);
+            count = try self.blinks(value / div, depth - 1) + try self.blinks(value % div, depth - 1);
+        } else {
+            count = try self.blinks(value * 2024, depth - 1);
+        }
+        
+        try self.rocks.put(.{ .value = value, .depth = depth }, count);
+
+        return count;
+    }
+};
+
+fn solve(comptime count: u64, file: std.fs.File, allocator: Allocator) !u64 {
     var buffered_reader = std.io.bufferedReader(file.reader());
     const reader = buffered_reader.reader();
     const stat = try file.stat();
 
-    var list = std.ArrayList(u64).init(allocator);
-    defer list.deinit();
-
     const input = (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', stat.size)) orelse return error.EmptyFile;
     defer allocator.free(input);
 
+    var solver: Solver = Solver.init(allocator);
+    defer solver.deinit();
+
     var split = std.mem.splitAny(u8, input, " ");
+    var sum: u64 = 0;
     while (split.next()) |num| {
-        try list.append(try std.fmt.parseInt(u64, num, 10));
+        const number = try std.fmt.parseInt(u64, num, 10);
+        sum += try solver.blinks(number, count);
     }
 
-    var count: u64 = 0;
-    while (count < 25) : (count += 1) {
-        var i: u64 = 0;
-        while (i < list.items.len) : (i += 1) {
-            if (list.items[i] == 0) {
-                list.items[i] = 1;
-            } else if ((std.math.log10_int(list.items[i]) + 1) % 2 == 0) {
-                const copy = list.items[i];
-                list.items[i] = copy / try std.math.powi(u64, 10, (std.math.log10_int(copy)+1)/2);
-                try list.insert(i+1, copy % try std.math.powi(u64, 10, (std.math.log10_int(copy)+1)/2));
-                i += 1;
-            } else {
-                list.items[i] *= 2024;
-            }
-        }
-    }
+    return sum;
+}
 
-    return list.items.len;
+fn part1(file: std.fs.File, allocator: Allocator) !u64 {
+    return solve(25, file, allocator);
 }
 
 fn part2(file: std.fs.File, allocator: Allocator) !u64 {
-    _ = allocator; // autofix
-    _ = file; // autofix
-    return 0;
+    return solve(75, file, allocator);
 }
 
 pub fn main() !void {
