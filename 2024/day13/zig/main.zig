cafe06fcc0b4b2b7f018e53298f25f093d756c8d
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-13 13:19:27 +0100

    Zig Day 13 2024 part 2

--- 2024/day13/zig/main.zig
+++ 2024/day13/zig/main.zig
@@ -2,22 +2,19 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 
 const Vec2 = struct {
-    x: i64,
-    y: i64,
-    fn mul(self: @This(), lambda: i64) @This() {
+    x: f64,
+    y: f64,
+    fn mul(self: @This(), lambda: f64) @This() {
         return .{
             .x = self.x * lambda,
             .y = self.y * lambda,
         };
     }
-    fn sub(self: @This(), other: @This()) @This() {
+    fn add(self: @This(), other: @This()) @This() {
         return .{
-            .x = self.x - other.x,
-            .y = self.y - other.y,
+            .x = self.x + other.x,
+            .y = self.y + other.y,
         };
-    }
-    fn is_zero(self: @This()) bool {
-        return self.x == 0 and self.y == 0;
     }
     pub fn format(
         self: @This(),
@@ -27,7 +24,42 @@ const Vec2 = struct {
     ) !void {
         _ = fmt;
         _ = options;
-        try writer.print("({} {})", .{ self.x, self.y });
+        try writer.print("({d: >7.2} {d: >7.2})", .{ self.x, self.y });
+    }
+};
+
+const Mat2 = struct {
+    a: f64,
+    b: f64,
+    c: f64,
+    d: f64,
+    fn det(self: @This()) f64 {
+        return (self.a * self.d - self.b * self.c);
+    }
+    fn inverse(self: @This()) @This() {
+        const d = self.det();
+        return .{
+            .a = self.d / d,
+            .b = -self.b / d,
+            .c = -self.c / d,
+            .d = self.a / d,
+        };
+    }
+    fn mul(self: @This(), other: Vec2) Vec2 {
+        return .{
+            .x = self.a * other.x + self.b * other.y,
+            .y = self.c * other.x + self.d * other.y,
+        };
+    }
+    pub fn format(
+        self: @This(),
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) !void {
+        _ = fmt;
+        _ = options;
+        try writer.print("|{d: >7.4} {d: >7.4}|\n|{d: >7.4} {d: >7.4}|", .{ self.a, self.b, self.c, self.d });
     }
 };
 
@@ -43,12 +75,12 @@ const ClawMachine = struct {
                 _ = data.next();
                 var vec: Vec2 = undefined;
                 if (data.next()) |line| {
-                    vec.x = try std.fmt.parseInt(i64, line[3..], 10);
+                    vec.x = try std.fmt.parseFloat(f64, line[3..]);
                 } else {
                     return error.InsufficientNumberOfValues;
                 }
                 if (data.next()) |line| {
-                    vec.y = try std.fmt.parseInt(i64, line[3..], 10);
+                    vec.y = try std.fmt.parseFloat(f64, line[3..]);
                 } else {
                     return error.InsufficientNumberOfValues;
                 }
@@ -70,22 +102,16 @@ const ClawMachine = struct {
         return self;
     }
 
-    fn find_optimal_inputs(self: *const @This(), comptime max_iterations: u64) ?u64 {
-        var cost: u64 = std.math.maxInt(u64);
-        var found = false;
-        var i: u64 = 0;
-        outer: while (i < max_iterations) : (i += 1) {
-            var j: u64 = 0;
-            while (j < max_iterations) : (j += 1) {
-                if (self.target.sub(self.a_button.mul(@intCast(i))).sub(self.b_button.mul(@intCast(j))).is_zero()) {
-                    if (cost > (i * 3 + j)) cost = i * 3 + j;
-                    found = true;
-                    continue :outer;
-                }
-            }
-        }
-        if (found) return cost;
-        return null;
+    fn find_optimal_inputs(self: *const @This()) ?u64 {
+        const system = Mat2{ .a = self.a_button.x, .b = self.b_button.x, .c = self.a_button.y, .d = self.b_button.y };
+        const inv = system.inverse();
+        const solution_float = inv.mul(self.target);
+        const solution = Vec2{ .x = @round(solution_float.x), .y = @round(solution_float.y) };
+
+        const target = self.a_button.mul(solution.x).add(self.b_button.mul(solution.y));
+        if (target.x != self.target.x or target.y != self.target.y) return null;
+
+        return @as(u64, @intFromFloat(solution.x)) * 3 + @as(u64, @intFromFloat(solution.y));
     }
 };
 
@@ -109,16 +135,38 @@ fn part1(file: std.fs.File, allocator: Allocator) !u64 {
 
     var sum: u64 = 0;
     for (machines.items) |machine| {
-        if (machine.find_optimal_inputs(100)) |cost| sum += cost;
+        if (machine.find_optimal_inputs()) |cost| sum += cost;
     }
 
     return sum;
 }
 
 fn part2(file: std.fs.File, allocator: Allocator) !u64 {
-    _ = allocator; // autofix
-    _ = file; // autofix
-    return 0;
+    var buffered_reader = std.io.bufferedReader(file.reader());
+    const reader = buffered_reader.reader();
+    const stat = try file.stat();
+
+    const content = try reader.readAllAlloc(allocator, stat.size);
+    defer allocator.free(content);
+
+    var lines = std.mem.splitAny(u8, content, "\n");
+
+    var machines = std.ArrayList(ClawMachine).init(allocator);
+    defer machines.deinit();
+
+    while (lines.peek()) |_| {
+        var machine = try ClawMachine.init(&lines);
+        machine.target = machine.target.add(.{ .x = 10000000000000, .y = 10000000000000 });
+        try machines.append(machine);
+        _ = lines.next(); // discard empty line
+    }
+
+    var sum: u64 = 0;
+    for (machines.items) |machine| {
+        if (machine.find_optimal_inputs()) |cost| sum += cost;
+    }
+
+    return sum;
 }
 
 pub fn main() !void {
