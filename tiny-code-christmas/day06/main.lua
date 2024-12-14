2dbd140cfe246371dd9059f229da53e62d522359
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-14 15:00:31 +0100

    TCC Day 6: Repeating sine scroller with tunnel and multiple layers in 255 characters.

---
+++ tiny-code-christmas/day06/main.lua
@@ -0,0 +1,30 @@
+TIC=load"t=t+1 for i=0,32639 do x=i%240-120 y=i/240-68 X=6*math.atan(y,x)Y=99/(x*x+y*y)^.5+t/4 poke4(i,X//1~Y//1)end for j=0,2 do for i=1,#m do print(string.sub(m,i,i),(i*18-t-j)%480,60+math.sin(i+t/4)*10,j,true,3)end end"m="MERRY TINY CODE CHRISTMAS!"t=0
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
