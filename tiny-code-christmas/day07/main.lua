5876af12d531fa688907af5548f0d82c8ec56180
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-15 18:35:08 +0100

    TCC Day 7: Dithered Plasma with Stacked-Scroller using multiple sines.

---
+++ tiny-code-christmas/day07/main.lua
@@ -0,0 +1,41 @@
+m="MERRY TINY CODE CHRISTMAS!"t=0
+function TIC()
+t=t+.5
+for i=0,32639 do
+c=s(t/10)*s(i%240/40)+s(i/9600)
+poke4(i,c*8+i^2.5%1)end
+for j=0,3 do
+for i=1,#m do
+print(m:sub(i,i),(i*18-t-j)%500,s(t/10-i+8)*20+60+s(i+t/4)*10,j+t/5,true,3)end
+end
+end
+s=math.sin
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
