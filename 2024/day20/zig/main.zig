63ca4e295040153157eb61f227afa49db99191cf
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-20 23:59:28 +0100

    Zig Day 20 2024 both parts

--- 2024/day20/zig/main.zig
+++ 2024/day20/zig/main.zig
@@ -32,6 +32,10 @@ const Solver = struct {
             return .{ .x = self.x + other.x, .y = self.y + other.y };
         }
 
+        fn manhatten(self: @This(), other: @This()) u64 {
+            return @abs(self.x - other.x) + @abs(self.y - other.y);
+        }
+
         pub fn format(
             self: @This(),
             comptime fmt: []const u8,
@@ -44,7 +48,7 @@ const Solver = struct {
         }
     };
     allocator: Allocator,
-    map: std.AutoArrayHashMap(Vec2, Tile),
+    map: []Vec2,
     start: Vec2,
     end: Vec2,
     width: usize,
@@ -53,11 +57,13 @@ const Solver = struct {
     fn init(file: File, allocator: Allocator) !@This() {
         var self: @This() = undefined;
         self.allocator = allocator;
-        self.map = std.AutoArrayHashMap(Vec2, Tile).init(self.allocator);
 
         var buffered_reader = std.io.bufferedReader(file.reader());
         const reader = buffered_reader.reader();
         const stat = try file.stat();
+
+        var map = std.AutoArrayHashMap(Vec2, Tile).init(allocator);
+        defer map.deinit();
 
         var width: usize = 0;
         var height: usize = 0;
@@ -70,25 +76,27 @@ const Solver = struct {
                     'S', 'E', '.' => {
                         if (c == 'S') self.start = p;
                         if (c == 'E') self.end = p;
-                        try self.map.put(p, .{ .variant = .PATH, .index = null });
+                        try map.put(p, .{ .variant = .PATH, .index = null });
                     },
                     '#' => {
-                        try self.map.put(p, .{ .variant = .WALL, .index = null });
+                        try map.put(p, .{ .variant = .WALL, .index = null });
                     },
                     else => {},
                 }
             }
             height += 1;
         }
-        self.width = width;
-        self.height = height;
+
+        var path = std.ArrayList(Vec2).init(allocator);
+        defer path.deinit();
 
         var length: usize = 0;
         var current = self.start;
-        while (current.x == self.end.x and current.y == self.end.y) : (length += 1) {
-            try self.map.put(current, .{ .variant = .PATH, .index = length });
+        while (current.x != self.end.x or current.y != self.end.y) : (length += 1) {
+            try path.append(current);
+            try map.put(current, .{ .variant = .PATH, .index = length });
             for (Direction.ALL) |dir| {
-                if (self.map.get(current.add(Vec2.fromDirection(dir)))) |next| {
+                if (map.get(current.add(Vec2.fromDirection(dir)))) |next| {
                     if (next.variant == .PATH and next.index == null) {
                         current = current.add(Vec2.fromDirection(dir));
                         break;
@@ -96,19 +104,59 @@ const Solver = struct {
                 }
             }
         }
+        try path.append(current);
+        try map.put(current, .{ .variant = .PATH, .index = length });
+
+        self.map = try path.toOwnedSlice();
 
         return self;
     }
 
     fn deinit(self: *@This()) void {
-        self.map.deinit();
+        self.allocator.free(self.map);
+    }
+
+    fn find_cheats(self: *const @This(), allocator: Allocator, comptime max_length: usize) !std.AutoArrayHashMap(usize, usize) {
+        var result = std.AutoArrayHashMap(usize, usize).init(allocator);
+
+        for (self.map, 0..) |p1, i| {
+            for (self.map[(i+1)..], (i+1)..) |p2, j| {
+                const manhatten = p1.manhatten(p2);
+                if (manhatten > max_length) continue;
+                const timesave = j - i - manhatten;
+                if (result.getPtr(timesave)) |ptr| {
+                    ptr.* += 1;
+                } else {
+                    try result.put(timesave, 1);
+                }
+            }
+        }
+
+        return result;
     }
 };
 
-fn part1(file: File, allocator: Allocator) !u64 {
+fn part(file: File, allocator: Allocator, comptime target: usize, comptime max_length: usize) !u64 {
     var solver = try Solver.init(file, allocator);
     defer solver.deinit();
-    return 0;
+
+    var r = try solver.find_cheats(allocator, max_length);
+    defer r.deinit();
+
+    var sum: u64 = 0;
+    for (r.keys()) |k| {
+        if (k >= target) sum += r.get(k).?;
+    }
+
+    return sum;
+}
+
+fn part1(file: File, allocator: Allocator, comptime target: usize) !u64 {
+    return try part(file, allocator, target, 2);
+}
+
+fn part2(file: File, allocator: Allocator, comptime target: usize) !u64 {
+    return try part(file, allocator, target, 20);
 }
 
 pub fn main() !void {
@@ -123,7 +171,12 @@ pub fn main() !void {
     {
         const file = try std.fs.cwd().openFile(filename, .{ .mode = .read_only });
         defer file.close();
-        std.debug.print("Part 1: {}\n", .{try part1(file, allocator)});
+        std.debug.print("Part 1: {}\n", .{try part1(file, allocator, 100)});
+    }
+    {
+        const file = try std.fs.cwd().openFile(filename, .{ .mode = .read_only });
+        defer file.close();
+        std.debug.print("Part 2: {}\n", .{try part2(file, allocator, 100)});
     }
 }
 
@@ -131,6 +184,14 @@ test "part 1" {
     const file = std.fs.cwd().openFile("../../../inputs/2024/day20/test.txt", .{ .mode = .read_only }) catch return error.FileNotFound;
     defer file.close();
     const allocator = std.testing.allocator;
-    const result = part1(file, allocator);
-    try std.testing.expectEqual(0, result);
+    const result = part1(file, allocator, 40);
+    try std.testing.expectEqual(2, result);
+}
+
+test "part 2" {
+    const file = std.fs.cwd().openFile("../../../inputs/2024/day20/test.txt", .{ .mode = .read_only }) catch return error.FileNotFound;
+    defer file.close();
+    const allocator = std.testing.allocator;
+    const result = part2(file, allocator, 70);
+    try std.testing.expectEqual(41, result);
 }
