c98ee90bdef7c92b7e84f8e005f120ac7b48a9dc
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2024-12-22 16:52:30 +0100

    Zig Day 22 2024 part 2

--- 2024/day22/zig/main.zig
+++ 2024/day22/zig/main.zig
@@ -31,22 +31,43 @@ fn part2(file: File, allocator: Allocator) !u64 {
     const reader  = buffered_reader.reader();
     const stat = try file.stat();
 
-    var sum: usize = 0;
+    var cache = std.AutoHashMap([4]i64, u64).init(allocator);
+    defer cache.deinit();
+
     while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', stat.size)) |line| {
         defer allocator.free(line);
         var secret = try std.fmt.parseInt(usize, line, 10);
-        var changes: [1999]i64 = undefined;
+        var seen = std.AutoHashMap([4]i64, bool).init(allocator);
+        defer seen.deinit();
+        var seq = [_]i64{ 10 } ** 4;
 
         var i: usize = 0;
         while (i < 2000) : (i += 1) {
-            const new = findSecretNumber(secret);
-            changes[i] = @as(i64, @intCast(new % 10)) - @as(i64, @intCast(secret % 10));
-            secret = new;
+            const previous = secret % 10;
+            secret = findSecretNumber(secret);
+            const current = secret % 10;
+
+            inline for (seq[0..3], 0..) |_, j| seq[j] = seq[j+1];
+            seq[3] = @as(i64, @intCast(current)) - @as(i64, @intCast(previous));
+
+            if (seen.get(seq)) |_| continue;
+
+            try seen.put(seq, true);
+            if (cache.getPtr(seq)) |ptr| {
+                ptr.* += current;
+            } else {
+                try cache.put(seq, current);
+            }
         }
-        sum += secret;
     }
 
-    return sum;
+    var max: u64 = 0;
+    var it = cache.valueIterator();
+    while (it.next()) |v| {
+        if (v.* > max) max = v.*;
+    }
+
+    return max;
 }
 
 pub fn main() !void {
@@ -63,6 +84,11 @@ pub fn main() !void {
         defer file.close();
         std.debug.print("Part 1: {}\n", .{try part1(file, allocator)});
     }
+    {
+        const file = try std.fs.cwd().openFile(filename, .{ .mode = .read_only });
+        defer file.close();
+        std.debug.print("Part 2: {}\n", .{try part2(file, allocator)});
+    }
 }
 
 test "part 1" {
@@ -74,7 +100,7 @@ test "part 1" {
 }
 
 test "part 2" {
-    const file = std.fs.cwd().openFile("../../../inputs/2024/day22/test.txt", .{ .mode = .read_only }) catch return error.FileNotFound;
+    const file = std.fs.cwd().openFile("../../../inputs/2024/day22/test_2.txt", .{ .mode = .read_only }) catch return error.FileNotFound;
     defer file.close();
     const allocator = std.testing.allocator;
     const result = part2(file, allocator);
