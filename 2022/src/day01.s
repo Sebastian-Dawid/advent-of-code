9e904a99462f929b310be758d4a1e648494135df
Sebastian Emanuel Dawid <sdawid@techfak.uni-bielefeld.de>
2025-11-20 12:29:02 +0100

    Finish assembly solution for day 1 of 2022

--- 2022/src/day01.s
+++ 2022/src/day01.s
@@ -16,6 +16,7 @@
 # Allocate %rdi bytes.
 # The address of the allocation is returned in %rax.
 alloc:
+	pushq	%rdi
 	pushq	%rsi
 	pushq	%rdx
 	pushq	%r10
@@ -25,23 +26,35 @@ alloc:
 	movq	$SYS_MMAP,	%rax
 	# size
 	movq	%rdi,	%rsi
+	addq	$8,	%rsi
 	# addr
 	movq	$0,	%rdi
 	# PROT_READ | PROT_WRITE
 	movq	$3,	%rdx
 	# MAP_PRIVATE | MAP_ANONYMOUS
-	movq	0x22,	%r10
+	movq	$34,	%r10
 	# No filedescriptor
-	movq	-1,	%r8
+	movq	$-1,	%r8
 	# offset 0
 	movq	$0,	%r9
 	syscall
+
+	subq	$8,	%rsi
+	movq	%rsi,	(%rax)
+
+	movq	$0,	%rdi
+alloc.loop.zero:
+	movb	$0,	8(%rax, %rdi)
+	incq	%rdi
+	cmpq	%rsi,	%rdi
+	jne	alloc.loop.zero
 
 	popq	%r9
 	popq	%r8
 	popq	%r10
 	popq	%rdx
 	popq	%rsi
+	popq	%rdi
 	ret
 
 # Print the number stored in %rdi to STDOUT
@@ -63,14 +76,14 @@ printNumber:
 	movq	%rdi,	%rax
 
 	# Add a newline to the end of the string
-	subq	$1,	%rsp
-	movb	$0xA,	(%rsp)
+	subq	$8,	%rsp
+	movq	$0xA,	(%rsp)
 
 	# do {
 printNumber.loop:
 	# value / 10 = 10 * result + remainder
 	movq	$0,	%rdx
-	divq	-1(%rbp)
+	divq	-8(%rbp)
 	
 	# Append a character to the front of the string
 	subq	$1,	%rsp
@@ -97,8 +110,10 @@ printNumber.loop:
 	ret
 
 # Parse the number in the string at %rdi
-# The number of digits in the number is given in %rsi
+# The termination character is stored in %rsi
+# A pointer to where to store the number of read characters is stored in %rdx.
 # The parsed number is written to %rax
+# If the given string is not a number %rax will be set to ~0
 parseNumber:
 	pushq	%rbp
 	movq	%rsp,	%rbp
@@ -107,13 +122,36 @@ parseNumber:
 
 	pushq	%rbx
 	pushq	%rcx
-	pushq	%rdx
 	
+	# i = 0
+	movq	$0,	%rcx
+	# while (str[i] != terminator) {
+parseNumber.count.loop:
+	cmpb	(%rdi,	%rcx),	%sil
+	je	parseNumber.count.loopEnd
+	# i++
+	incq	%rcx
+	jmp	parseNumber.count.loop
+	# }
+parseNumber.count.loopEnd:
+	# count = i+1
+	leaq	1(%rcx),	%rbx
+	movq	%rbx,	(%rdx)
+
+	# if (i == 0) {
+	cmp	$0,	%rcx
+	jne	parseNumber.count.valid
+	# return ~0
+	notq	%rcx
+	jmp	parseNumber.postamble
+	# }
+parseNumber.count.valid:
+	# i = len
+	movq	%rcx,	%rsi
 	# result = 0
 	movq	$0,	%rcx
 	# digit = 1
 	movq	$1,	%rax
-	# i = len
 	# do {
 parseNumber.parse:
 	# i--
@@ -121,31 +159,148 @@ parseNumber.parse:
 	# val = str[i] - 0x30
 	movzbq	(%rdi, %rsi),	%rbx
 	subb	$0x30,	%bl
-	
 	# result += digit * val
 	imulq	%rax,	%rbx
 	addq	%rbx,	%rcx
-
 	# digit *= 10
 	mulq	-8(%rbp)
 	# } while(i > 0);
 	cmp	$0,	%rsi
 	jne	parseNumber.parse
-
+parseNumber.postamble:
 	movq	%rcx,	%rax
-	popq	%rdx
 	popq	%rcx
 	popq	%rbx
 	leave
 	ret
 
-# Parse the data at %rax. The filesize is passed through %rbx
+# Parse the data at %rdi. The filesize is passed through %rsi
+# %rsi and %rsi are clobbered
 # Return address of sorted list of sums in %rax
 parseData:
+	pushq	%rbp
+	movq	%rsp,	%rbp
+	subq	$32,	%rsp
+	# address of file contents
+	movq	%rdi,	-8(%rbp)
+	# size of the file
+	movq	%rsi,	-16(%rbp)
+	# address of the allocated list
+	movq	$0,	-24(%rbp)
+	# number of read characters
+	movq	$0,	-32(%rbp)
+	movq	%rdi,	%rsi
+
+	pushq	%rbx
+	pushq	%rcx
+	pushq	%rdx
+
 	# Determine number of elements
+	# i = 0
+	movq	$0,	%rcx
+
+	# count = 0
+	movq	$0,	%rbx
+	# do {
+parseData.count.loop:
+	# if (str[i] == '\n') {
+	movb	(%rsi, %rcx),	%dl
+	cmpb	$0xA,	%dl
+	jne	parseData.count.loop.noLinebreak
+	# count += 8
+	addq	$8,	%rbx
+	# }
+parseData.count.loop.noLinebreak:
+	# i++
+	incq	%rcx
+	# } while (i < filesize);
+	cmp	-16(%rbp),	%rcx
+	jne	parseData.count.loop
+	# if (str[end] != '\n') {
+	movb	-1(%rsi, %rcx),	%dl
+	cmpb	$0xA,	%dl
+	je parseData.count.fileEndLinebreak
+	# count += 8
+	addq	$8,	%rbx
+	# }
+parseData.count.fileEndLinebreak:
+
 	# Alloc buffer
-	# Sum up until empty line or EOF
-	# Add to buffer in the appropriate spot
+	movq	%rbx,	%rdi
+	call	alloc
+
+	movq	%rax,	-24(%rbp)
+
+	# i = 0
+	movq	$0,	%rcx
+	# str
+	movq	-8(%rbp),	%rdi
+	# total = 0
+	movq	$0,	%rbx
+	# do {
+parseData.sum.loop:
+	# num = parseNumber(str, '\n', &count)
+	movq	$0xA,	%rsi
+	leaq	-32(%rbp),	%rdx
+	call	parseNumber
+	# str += count
+	addq	-32(%rbp),	%rdi
+	# i += count
+	addq	-32(%rbp),	%rcx
+	# if (num != -1) {
+	cmpq	$-1,	%rax
+	je	parseData.sum.loop.empty
+	# total += num
+	addq	%rax,	%rbx
+	jmp	parseData.sum.loop.postamble
+	# } else {
+parseData.sum.loop.empty:
+	# Insert total at the correct spot in the allocated buffer
+	# The buffer is sorted descending
+	
+	pushq	%rcx
+	pushq	%rdx
+
+	# buf
+	movq	-24(%rbp),	%rsi
+	# len = length(buf)
+	movq	(%rsi),	%rdx
+	# i = 0
+	movq	$0,	%rcx
+	# do {
+parseData.sum.loop.empty.loop:
+	# if (buf[i] < v) {
+	cmpq	%rbx, 8(%rsi, %rcx)
+	jge	parseData.sum.loop.empty.loop.noSwitch
+	# switch(buf[i], v);
+	xorq	8(%rsi, %rcx),	%rbx
+	xorq	%rbx,	8(%rsi, %rcx)
+	xorq	8(%rsi, %rcx),	%rbx
+	# }
+parseData.sum.loop.empty.loop.noSwitch:
+	# i++
+	addq	$8,	%rcx
+	# } while (i < len);
+	cmp	%rcx,	%rdx
+	jne	parseData.sum.loop.empty.loop
+
+	popq	%rdx
+	popq	%rcx
+
+	# total = 0
+	movq	$0,	%rbx
+	# }
+parseData.sum.loop.postamble:
+	# } while (i < filesize);
+	cmp	-16(%rbp),	%rcx
+	jne	parseData.sum.loop
+
+parseData.postamble:
+	movq	-24(%rbp),	%rax
+	popq	%rdx
+	popq	%rcx
+	popq	%rbx
+	leave
 	ret
 
 _start:
@@ -180,12 +335,23 @@ _start:
 	movq	%r8,	%rdi
 	syscall
 
-	movq	(%rsp),	%rdi
-	movq	$4,	%rsi
-	call	parseNumber
+	popq	%rdi
+	movq	0x30(%rsp),	%rsi
+	call	parseData
 
-	movq	%rax,	%rdi
-	call printNumber
+	pushq	%rax
+
+	# part 1
+	movq	8(%rax),	%rdi
+	call	printNumber
+
+	popq	%rax
+
+	# part 2
+	movq	8(%rax),	%rdi
+	addq	16(%rax),	%rdi
+	addq	24(%rax),	%rdi
+	call	printNumber
 
         mov $SYS_EXIT,	%rax
         mov $0, %rdi
